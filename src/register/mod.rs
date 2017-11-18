8a10a314dd1414b469bc5b3950b926714b05eab7
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-11-18 20:09:57 +0100

    Create Teams

--- src/register/mod.rs
+++ src/register/mod.rs
@@ -1,13 +1,66 @@
 use std::io::Read;
-use std::convert::Into;
+use std::convert::{From, Into};
+use std::collections::HashMap;
 
 use iron::{Request, Response, IronResult, status};
 use router::Router;
 use serde_json;
 use serde_json::{Error};
 
+pub struct Teams {
+    teams: HashMap<String, Team>,
+}
+
+impl Teams {
+    pub fn new() -> Teams {
+        Teams { teams: HashMap::new() }
+    }
+
+    pub fn register(&mut self, registration: Registration) -> RegistrationAttempt {
+        if self.teams.contains_key(&registration.name) {
+            return RegistrationAttempt::Failure(Reason::NameTaken);
+        }
+
+        for ip_address in self.ip_addresses() {
+            if ip_address == registration.ip_address {
+                return RegistrationAttempt::Failure(Reason::IPAddressTaken);
+            }
+        }
+
+        self.teams.insert(registration.name.clone(), Team::from(registration));
+        RegistrationAttempt::Success
+    }
+
+    fn ip_addresses(&self) -> Vec<&str> {
+        self.teams.iter().map(|team| &team.1.ip_address[..]).collect()
+    }
+}
+
+#[derive(PartialEq, Debug)]
+pub enum RegistrationAttempt {
+    Success,
+    Failure(Reason)
+}
+
+#[derive(PartialEq, Debug)]
+pub enum Reason {
+    NameTaken,
+    IPAddressTaken,
+}
+
+struct Team {
+    name: String,
+    ip_address: String,
+}
+
+impl From<Registration> for Team {
+    fn from(registration: Registration) -> Self {
+        Team { name: registration.name, ip_address: registration.ip_address }        
+    }
+}
+
 #[derive(Deserialize, Debug)]
-struct Registration {
+pub struct Registration {
     name: String,
     ip_address: String,
 }
@@ -53,5 +106,60 @@ fn handler(request: &mut Request) -> IronResult<Response> {
         let payload = serde_json::to_string(&reason).unwrap();
 
         Ok(Response::with((status::InternalServerError, payload)))
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn team_should_be_registered_with_a_registration() {
+        let mut teams = Teams::new();
+        let registration = Registration {
+            name : "TEST".to_owned(),
+            ip_address : "TEST ADDRESS".to_owned(),
+        };
+
+        let result = teams.register(registration);
+
+        assert_eq!(result, RegistrationAttempt::Success);
+    }
+
+    #[test]
+    fn team_with_same_name_should_not_be_registered() {
+        let mut teams = Teams::new();
+        let first = Registration {
+            name : "TEST".to_owned(),
+            ip_address : "TEST ADDRESS".to_owned(),
+        };
+        let _ = teams.register(first);
+
+        let second = Registration {
+            name : "TEST".to_owned(),
+            ip_address : "OTHER TEST ADDRESS".to_owned(),
+        };
+        let result = teams.register(second);
+
+        assert_eq!(result, RegistrationAttempt::Failure(Reason::NameTaken));
+    }
+
+    #[test]
+    fn team_with_same_ip_address_should_not_be_registered() {
+        let mut teams = Teams::new();
+        let first = Registration {
+            name : "TEST".to_owned(),
+            ip_address : "TEST ADDRESS".to_owned(),
+        };
+        let _ = teams.register(first);
+
+        let second = Registration {
+            name : "OTHER TEST".to_owned(),
+            ip_address : "TEST ADDRESS".to_owned(),
+        };
+        let result = teams.register(second);
+
+        assert_eq!(result, RegistrationAttempt::Failure(Reason::IPAddressTaken));
     }
 }
