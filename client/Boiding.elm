52e43fe8d10ce85a4798980c812baaa129e97c80
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-12-07 14:17:24 +0100

    Make elm client a Html.program

--- client/Boiding.elm
+++ client/Boiding.elm
@@ -4,4 +4,44 @@ import Html
 
 
 main =
-    Html.text "Hello, World"
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+init : ( Model, Cmd Message )
+init =
+    ( { sentence = "Hello, World" }, Cmd.none )
+
+
+type alias Model =
+    { sentence : String
+    }
+
+
+type Message
+    = Say String
+
+
+update : Message -> Model -> ( Model, Cmd Message )
+update message model =
+    case message of
+        Say sentence ->
+            ( { model | sentence = sentence }, Cmd.none )
+
+
+view : Model -> Html.Html Message
+view model =
+    Html.div []
+        [ Html.span []
+            [ Html.text model.sentence ]
+        ]
+
+
+
+subscriptions : Model -> Sub Message
+subscriptions model =
+    Sub.none
