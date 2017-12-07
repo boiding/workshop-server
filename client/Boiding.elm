c16923ebe77eb380f23c7e6b47c1c8df5aba0c37
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-12-07 15:22:09 +0100

    Show a list of teams

--- client/Boiding.elm
+++ client/Boiding.elm
@@ -1,6 +1,8 @@
 module Boiding exposing (..)
 
 import Html
+import Html.Attributes as Attribute
+import Dict
 
 
 main =
@@ -14,32 +16,65 @@ main =
 
 init : ( Model, Cmd Message )
 init =
-    ( { sentence = "Hello, World" }, Cmd.none )
+    let
+        teams =
+            Dict.empty
+                |> Dict.insert "red-bergen-crab" { name = "red-bergen-crab", connected = True }
+                |> Dict.insert "yellow-nijmegen-whale" { name = "yellow-nijmegen-whale", connected = False }
+    in
+        ( { team_repository = { teams = teams } }, Cmd.none )
 
 
 type alias Model =
-    { sentence : String
+    { team_repository : Teams
+    }
+
+
+type alias Teams =
+    { teams : Dict.Dict String Team
+    }
+
+
+type alias Team =
+    { name : String
+    , connected : Bool
     }
 
 
 type Message
-    = Say String
+    = DoNothing
 
 
 update : Message -> Model -> ( Model, Cmd Message )
 update message model =
     case message of
-        Say sentence ->
-            ( { model | sentence = sentence }, Cmd.none )
+        DoNothing ->
+            ( model, Cmd.none )
 
 
 view : Model -> Html.Html Message
 view model =
-    Html.div []
-        [ Html.span []
-            [ Html.text model.sentence ]
-        ]
+    let
+        teams =
+            model.team_repository
+                |> .teams
+                |> Dict.values
+                |> List.map viewTeam
+    in
+        Html.div [ Attribute.class "teams" ] teams
 
+
+viewTeam : Team -> Html.Html Message
+viewTeam team =
+    Html.div
+        [ Attribute.classList
+            [ ( "team", True )
+            , ( "disconnected", not team.connected )
+            , ( "connected", team.connected )
+            ]
+        ]
+        [ Html.span [] [ Html.text team.name ]
+        ]
 
 
 subscriptions : Model -> Sub Message
