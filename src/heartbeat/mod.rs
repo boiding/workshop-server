1b2a2c4438c85fea5ae0cf254c9c7f96989bbc86
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-12-09 16:19:55 +0100

    Don't unwrap message send from and received in heartbeat

--- src/heartbeat/mod.rs
+++ src/heartbeat/mod.rs
@@ -1,7 +1,7 @@
 pub mod communication;
 
 use std::env;
-use std::sync::{Arc,Mutex};
+use std::sync::{Arc, Mutex};
 use std::sync::mpsc::{Receiver, Sender};
 use std::thread;
 use std::time::Duration;
@@ -28,46 +28,62 @@ impl Heartbeat {
         let client = Client::new(&core.handle());
 
         let sleep_duration_value = u64::from_str_radix(
-            &env::var("heartbeat_sleep_duration")
-                .expect("\"heartbeat_sleep_duration\" in environment variables")
-                , 10).expect("\"heartbeat_sleep_duration\" to be u64");
+            &env::var("heartbeat_sleep_duration").expect(
+                "\"heartbeat_sleep_duration\" in environment variables",
+            ),
+            10,
+        ).expect("\"heartbeat_sleep_duration\" to be u64");
         let sleep_duration = Duration::from_secs(sleep_duration_value);
 
         let team_rx_mutex = Arc::new(Mutex::new(self.tx.clone()));
         loop {
             thread::sleep(sleep_duration);
-            self.tx.send(TeamsMessage::Heartbeat).unwrap();
+            if let Err(error) = self.tx.send(TeamsMessage::Heartbeat) {
+                error!("could not send heartbeat: {:?}", error);
+            } else {
+                if let Ok(message) = self.rx.recv() {
+                    match message {
+                        HeartbeatMessage::Check(servers) => {
+                            for (team_name, uri) in servers {
+                                info!("heartbeat for {} at {}", team_name, uri);
+                                let (success_team_rx_mutex, success_team_name) =
+                                    (team_rx_mutex.clone(), team_name.clone());
+                                let (failure_team_rx_mutex, failure_team_name) =
+                                    (team_rx_mutex.clone(), team_name.clone());
+                                let request = Request::new(Method::Head, uri);
+                                let work =
+                                    client
+                                        .request(request)
+                                        .map(move |response| {
+                                            info!("{} {}", success_team_name, response.status());
+                                            success_team_rx_mutex
+                                                .lock()
+                                                .unwrap()
+                                                .send(TeamsMessage::HeartbeatStatus(
+                                                    (success_team_name, true),
+                                                ))
+                                                .unwrap();
+                                        })
+                                        .map_err(move |_| {
+                                            error!("{} disconnected", failure_team_name);
+                                            failure_team_rx_mutex
+                                                .lock()
+                                                .unwrap()
+                                                .send(TeamsMessage::HeartbeatStatus(
+                                                    (failure_team_name, false),
+                                                ))
+                                                .unwrap();
+                                        });
 
-            let message = self.rx.recv().unwrap();
-            match message {
-                HeartbeatMessage::Check(servers) => {
-                    for (team_name, uri) in servers {
-                        info!("heartbeat for {} at {}", team_name, uri);
-                        let (success_team_rx_mutex, success_team_name) =
-                            (team_rx_mutex.clone(), team_name.clone());
-                        let (failure_team_rx_mutex, failure_team_name) =
-                            (team_rx_mutex.clone(), team_name.clone());
-                        let request = Request::new(Method::Head, uri);
-                        let work = client
-                            .request(request)
-                            .map(move |response|{
-                                info!("{} {}", success_team_name, response.status());
-                                success_team_rx_mutex
-                                    .lock().unwrap()
-                                    .send(TeamsMessage::HeartbeatStatus((success_team_name, true))).unwrap();
-                            })
-                            .map_err(move |_|{
-                                error!("{} disconnected", failure_team_name);
-                                failure_team_rx_mutex
-                                    .lock().unwrap()
-                                    .send(TeamsMessage::HeartbeatStatus((failure_team_name, false))).unwrap();
-                            });
-
-                        match core.run(work) {
-                            _ => () /* Everything is fine */
+                                match core.run(work) {
+                                    _ => (), /* Everything is fine */
+                                }
+                            }
                         }
                     }
-                },
+                } else {
+                    error!("could not receive message")
+                }
             }
         }
     }
