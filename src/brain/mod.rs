28bd9563bd90f493c45c96c973268bf46b5534bd
Daan van Berkel <daan.van.berkel@alliander.com>
2020-03-06 21:13:11 +0100

    read intent into a string

--- src/brain/mod.rs
+++ src/brain/mod.rs
@@ -3,7 +3,7 @@ pub mod model;
 
 use std::sync::mpsc::{Receiver, Sender};
 
-use futures::Future;
+use futures::{Future, stream::Stream};
 use hyper::{header::ContentType, Client, Method, Request};
 use tokio_core::reactor::Core;
 
@@ -36,7 +36,16 @@ impl Brain {
                             request.set_body(payload);
                             let work = client
                                 .request(request)
-                                .map(move |_response| {
+                                .and_then(|response|{
+                                    response.body().concat2()
+                                })
+                                .map(|chunk| String::from_utf8(chunk.to_vec()).unwrap(/* TODO: error handling */))
+                                .map(|source| {
+                                    //serde_json::from_str(&source).unwrap(/* TODO: error handling */);
+                                    info!("intent: {}", source);
+                                    source
+                                })
+                                .map(move |_intent| {
                                     info!("picked brain of {}", success_team_name);
                                     team_tx
                                         .send(TeamsMessage::BrainUpdate(success_team_name))
