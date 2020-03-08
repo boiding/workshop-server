d2dc406324a1670ffff2c752eda7bd26ba4c1aee
Jan-Erik Rediger <janerik@fnordig.de>
2020-03-08 11:59:12 +0100

    Use the same hostname the current site is using

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,4 @@
-(function(document, Elm, socket_port) {
+(function(document, Elm, socket_host, socket_port) {
     const container = document.getElementById('client-container');
     const app = Elm.Boiding.init({
         node: container
@@ -6,7 +6,7 @@
 
     window.app = app;
 
-    const socket_address = 'ws://127.0.0.1:' + socket_port;
+    const socket_address = `ws://${socket_host}:${socket_port}`;
     const socket = new WebSocket(socket_address);
     socket.addEventListener('message', function(event){
         app.ports.updateTeams.send(event.data);
@@ -15,4 +15,4 @@
     app.ports.spawn.subscribe(function(team){
         socket.send(JSON.stringify({'Spawn': {'team': team}}));
     })
-})(document, Elm, 3435);
+})(document, Elm, location.hostname, 3435);
